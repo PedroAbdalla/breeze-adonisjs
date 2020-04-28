ebe1a1a07729e1138fe1d0bb1680e18bda02fb18
Pedro Abdala <pedroabdalla@outlook.com>
2020-04-28 22:42:16 +0100

    validation users

--- start/routes.js
+++ start/routes.js
@@ -3,7 +3,7 @@
 const Route = use('Route')
 
 //post
-Route.post('users', 'UserController.store')
+Route.post('users', 'UserController.store').validator('User')
 Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.post('files', 'FileController.store')
